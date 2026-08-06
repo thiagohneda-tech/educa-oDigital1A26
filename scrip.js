96415071a9a71ffac79f41a9ea1176138b6fdecf
thiagohneda-tech <thiago.hneda@escola.pr.gov.br>
2026-08-06 10:19:40 -0300

    mudei

--- scrip.js
+++ scrip.js
@@ -1,17 +1,29 @@
  const botoes = document.querySelectorAll("button");
 
-    botoes.forEach(function (botao) {
-        let curtiu = false;
-        botao.addEventListener("click", botaoClicado);
-        function botaoClicado() {
-            console.log("fui clicado");
-            let texto = botao.querySelector("span");
-            if (curtiu === false) {
-                texto.textContent++;
-                curtiu = true;
-            } else {
-                texto.textContent--;
-                curtiu = false;
-            }
-        }
-    })
+botoes.forEach(function (botao) {
+  let curtiu = false;
+  botao.addEventListener("click", botaoClicado);
+  function botaoClicado() {
+    let texto = botao.querySelector("span");
+    if (curtiu === false) {
+      texto.textContent++;
+      curtiu = true;
+    } else {
+      texto.textContent--;
+      curtiu = false;
+    }
+  }
+});
+
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+btnTemaEscuro.addEventListener("click", mudaTema);
+
+function mudaTema() {
+  const corpoPagina = document.body;
+  if (corpoPagina.classList.contains("tema-escuro")) {
+    corpoPagina.classList.remove("tema-escuro");
+  } else {
+    corpoPagina.classList.add("tema-escuro");
+  }
+}
