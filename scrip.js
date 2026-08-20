433ff00d6ec584d013a2ddf03abdafa87e3e96a8
thiagohneda-tech <thiago.hneda@escola.pr.gov.br>
2026-08-20 10:12:25 -0300

    jjjjj

--- scrip.js
+++ scrip.js
@@ -1,18 +1,26 @@
  const botoes = document.querySelectorAll("button");
 
 botoes.forEach(function (botao) {
-  let curtiu = false;
-  botao.addEventListener("click", botaoClicado);
-  function botaoClicado() {
-    let texto = botao.querySelector("span");
-    if (curtiu === false) {
-      texto.textContent++;
-      curtiu = true;
-    } else {
-      texto.textContent--;
-      curtiu = false;
+    // Ignora o botão do tema
+    if (botao.classList.contains("btn-tema-escuro")) {
+        return;
     }
-  }
+
+    let curtiu = false;
+
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu = false;
+        }
+    }
 });
 
 const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
@@ -20,10 +28,11 @@ const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 btnTemaEscuro.addEventListener("click", mudaTema);
 
 function mudaTema() {
-  const corpoPagina = document.body;
-  if (corpoPagina.classList.contains("tema-escuro")) {
-    corpoPagina.classList.remove("tema-escuro");
-  } else {
-    corpoPagina.classList.add("tema-escuro");
-  }
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
 }
