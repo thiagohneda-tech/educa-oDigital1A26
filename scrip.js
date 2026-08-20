03f20f9faaf1e61a9b73028d4132e232ceeb3dce
thiagohneda-tech <thiago.hneda@escola.pr.gov.br>
2026-08-20 10:16:43 -0300

    botao

--- scrip.js
+++ scrip.js
@@ -23,16 +23,15 @@ botoes.forEach(function (botao) {
     }
 });
 
-const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+const btnTemaEscuro =document.querySelector(".btn-tema-escuro");
 
 btnTemaEscuro.addEventListener("click", mudaTema);
 
-function mudaTema() {
+function  mudaTema() {
     const corpoPagina = document.body;
-
-    if (corpoPagina.classList.contains("tema-escuro")) {
-        corpoPagina.classList.remove("tema-escuro");
-    } else {
-        corpoPagina.classList.add("tema-escuro");
+if (corpoPagina.classList.contains("tema-escuro")) {
+    corpoPagina.classList.remove("tema-escuro");
+} else {
+    corpoPagina.classList.add("tema-escuro");
     }
 }
