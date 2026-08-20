c707dbcef8d67fa6ad1fc5b9197b5b87adecc224
thiagohneda-tech <thiago.hneda@escola.pr.gov.br>
2026-08-20 10:30:07 -0300

    alterei

--- scrip.js
+++ scrip.js
@@ -1,32 +1,29 @@
- const botoes = document.querySelectorAll("button");
+  const botoes = document.querySelectorAll("button");
 
-        botoes.forEach(function (botao) {
-            let curtiu = false;
+botoes.forEach(function (botao) {
+  let curtiu = false;
+  botao.addEventListener("click", botaoClicado);
+  function botaoClicado() {
+    let texto = botao.querySelector("span");
+    if (curtiu === false) {
+      texto.textContent++;
+      curtiu = true;
+    } else {
+      texto.textContent--;
+      curtiu = false;
+    }
+  }
+});
 
-            botao.addEventListener("click", botaoClicado);
-
-            function botaoClicado() {
-                let texto = botao.querySelector("span");
-
-                if (curtiu === false) {
-                    texto.textContent++;
-                    curtiu = true;
-                } else {
-                    texto.textContent--;
-                    curtiu = false;
-                }
-            }
-        });
-
-const btnTemaEscuro =document.querySelector(".btn-tema-escuro");
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
 btnTemaEscuro.addEventListener("click", mudaTema);
 
-function  mudaTema() {
-    const corpoPagina = document.body;
-if (corpoPagina.classList.contains("tema-escuro")) {
+function mudaTema() {
+  const corpoPagina = document.body;
+  if (corpoPagina.classList.contains("tema-escuro")) {
     corpoPagina.classList.remove("tema-escuro");
-} else {
+  } else {
     corpoPagina.classList.add("tema-escuro");
-    }
+  }
 }
